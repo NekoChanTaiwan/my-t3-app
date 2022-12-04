c4d4a6b35ba415d9ef45f4789d6ca00d74826e5e
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-12-04 02:25:21 -0800

    initial t3 app

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import('./src/env/server.mjs'))
+
+import nextBundleAnalyzer from '@next/bundle-analyzer'
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+}
+
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+export default withBundleAnalyzer(config)
